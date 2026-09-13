dc2103eab04c39c64eabe5355cbef52ccc9be726
Ani-matsu <Jacoby1992@yahoo.com>
2026-09-13 13:01:44 -0500

    Add files via upload
    MODIFIED:
    database/Database.java
    guiAdminHome/ControllerAdminHome.java
    guiUserLogin/ControllerUserLogin.java
    
    ADDED:
    guiOneTimePassword/ViewOneTimePassword.java
    guiOneTimePassword/ControllerOneTimePassword.java
    guiOneTimePassword/ModelOneTimePassword.java
    
    guiResetPassword/ViewResetPassword.java
    guiResetPassword/ControllerResetPassword.java
    guiResetPassword/ModelResetPassword.java

---
+++ FoundationsF26/src/guiResetPassword/ModelResetPassword.java
@@ -0,0 +1,18 @@
+package guiResetPassword;
+
+/*******
+ * <p> Title: ModelResetPassword Class. </p>
+ * 
+ * <p> Description: The Reset Password Page Model.  This class is not used as there is no
+ * data manipulated by this MVC beyond accepting the new password and saving it in the
+ * database.</p>
+ * 
+ * @author Team
+ * 
+ * @version 1.00		2026-09-13 Initial version
+ *  
+ */
+
+public class ModelResetPassword {
+
+}
